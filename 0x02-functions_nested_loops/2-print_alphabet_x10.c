836a988989158d32648c40671fc1e1474545aac1
Angela-Nyaka <nyakaangela@gmail.com>
2023-10-10 12:38:16 -0700

    print lower alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"                                                                                                                       
+/**
+* print_alphabet- prints the alphabet, in lowercase, followed by a new line
+*
+* Return : nothing
+*/
+  
+void print_alphabet_x10(void)
+{
+int i,j;
+
+j = 0;
+  
+while (j < 10)
+{
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+j++;
+}
+}
