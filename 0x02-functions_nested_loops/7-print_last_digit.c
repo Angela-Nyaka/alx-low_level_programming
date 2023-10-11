80626bd239c25e03282229a1aaba4271e2171e94
Angela-Nyaka <nyakaangela@gmail.com>
2023-10-10 20:20:31 -0700

    add task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_last_digit- prints last digit
+*@n:last digit
+*
+*Return: i
+*/
+int print_last_digit(int n)
+{
+int i;
+
+if (n < 0)
+n = -n;
+i = n % 10;
+_putchar(i + '0');
+return (i);
+}
