2cab61dd0cd5d8b4f6b659a43ab6a4148f0d48ab
Angela-Nyaka <nyakaangela@gmail.com>
2023-10-10 19:59:07 -0700

    add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"                                                                                                                       
+/**
+* abs- prints the alphabet, in lowercase, followed by a new line
+*@n: int to check
+* Return: absolute value
+*/
+int _abs(int n)
+{
+if (n >=  0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
+
