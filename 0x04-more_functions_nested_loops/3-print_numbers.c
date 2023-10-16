af227b99ee67d074c14c3c8c91c6bbd306ca8de6
Angela-Nyaka <nyakaangela@gmail.com>
2023-10-16 07:07:31 -0700

    function that writes 0 to 9 followed by new line using putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_numbers- prints number 0 to 9 followed by a new line
+*/
+void print_numbers(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
