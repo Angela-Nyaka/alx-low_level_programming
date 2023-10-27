3cee7c65b2adfe3e349e9b33d8ea4068a8f91bb3
Angela-Nyaka <nyakaangela@gmail.com>
2023-10-27 02:27:56 -0700

    fix betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,9 +2,9 @@
 
 int actual_prime(int n, int i);
 /**
-*is_prime_number- returns 1 if the input integer is a prime number, otherwise return 0.
-*n: integer
-*
+*is_prime_number- returns 1 if the input integer is a prime number
+*otherwise return 0.
+*@n: integer
 *Return: 1 if the input integer is a prime number, otherwise return 0.
 */
 int is_prime_number(int n)
@@ -16,7 +16,7 @@ return (actual_prime(n, n - 1));
 /**
 * actual_prime- calculate if number is prime
 *@n: number to evaluate
-*
+*@i: for looping
 *Return: 0 or 1
 */
 int actual_prime(int n, int i)
