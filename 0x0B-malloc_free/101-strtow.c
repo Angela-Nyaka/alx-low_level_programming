488f7b0ef01a3ea1b0197538a307943310f57c26
Angela-Nyaka <nyakaangela@gmail.com>
2023-11-02 02:52:25 -0700

    fix betty

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,7 +11,6 @@ int count_word(char *s)
 int flag, c, w;
 flag = 0;
 w = 0;
-		
 for (c = 0; s[c] != '\0'; c++)
 {
 	if (s[c] == ' ')
@@ -21,7 +20,6 @@ for (c = 0; s[c] != '\0'; c++)
 	flag = 1;
 	w++;
 	}
-		
 }
 return (w);
 }
@@ -37,10 +35,8 @@ char **strtow(char *str)
 char **matrix, *tmp;
 
 int i, k = 0, len = 0, words, c = 0, start, end;
-		
-while (*(str + len))
+while (*(str + len)
 len++;
-		
 words = count_word(str);
 if (words == 0)
 return (NULL);
@@ -63,11 +59,9 @@ while (start < end)
 matrix[k] = tmp - c;
 k++;
 c = 0;
-		
 }
 }
 else if (c++ == 0)
-		
 start = i;
 }
 matrix[k] = NULL;
