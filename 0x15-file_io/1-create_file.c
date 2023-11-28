a27788a4a0235199829d776f41ca158a712eaed6
Angela-Nyaka <nyakaangela@gmail.com>
2023-11-28 02:35:54 +0000

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
-* append_text_to_file - appends text at the end of a file
+* create_file - creates a file
 * @filename: filename.
-* @text_content: added content.
+* @text_content: content writed in the file.
 *
-* Return: 1 if the file exists. -1 if the fails does not exist
-* or if it fails.
+* Return: 1 if it success. -1 if it fails.
 */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 int fd;
 int nletters;
@@ -17,13 +16,14 @@ int rwr;
 if (!filename)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_APPEND);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 if (fd == -1)
 return (-1);
 
-if (text_content)
-{
+if (!text_content)
+text_content = "";
+
 for (nletters = 0; text_content[nletters]; nletters++)
 ;
 
@@ -31,7 +31,6 @@ rwr = write(fd, text_content, nletters);
 
 if (rwr == -1)
 return (-1);
-}
 
 close(fd);
 
