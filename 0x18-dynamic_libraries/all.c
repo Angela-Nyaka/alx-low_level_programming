651e7f2b6cefbfd8c03bc6510814afb54ca6df10
Angela-Nyaka <nyakaangela@gmail.com>
2024-03-30 06:24:23 +0000

    add task 3

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+/**
+*mul - multiplies two integers
+* @a: first integer
+*@b: second integer
+*Return: product of two integers
+*/
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+*add - add two integers
+* @a: first integer
+*@b: second integer
+*Return: addition
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*sub - subtracts two integers
+*@a: first integer
+*@b: second integer
+*Return: subtracts final resurt
+*/
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+*div - division of two integers
+* @a: first integer
+*@b: second integer
+*Return: division of two integers
+*/
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+*mod - module of two integers
+*@a: first integer
+*@b: second integer
+*Return: modulus
+*/
+int mod(int a, int b)
+{
+        return (a % b);
+}
