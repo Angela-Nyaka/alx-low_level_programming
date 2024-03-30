bb07091e8fc0b81479503213baa36fd2d37f77c8
Angela-Nyaka <nyakaangela@gmail.com>
2024-03-30 11:39:27 +0000

    update file

--- 0x18-dynamic_libraries/101-make-me-win.sh
+++ 0x18-dynamic_libraries/101-make-me-win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://raw.githubusercontent.com/Angela-Nyaka/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm_fucn.so
-export LD_PRELOAD="$PWD/../libgm_fcn.so"
+export LD_PRELOAD="$PWD/../libgm_fucn.so"
